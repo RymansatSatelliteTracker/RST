a4e7159e6376bc8a81389cff0aff7ff0f5802bd1
freefloatingplanet <suenaganu@gmail.com>
2025-04-05 18:26:22 +0900

    Hz対応の処置もれ

--- src/renderer/service/ActiveSatServiceHub.ts
+++ src/renderer/service/ActiveSatServiceHub.ts
@@ -1,6 +1,5 @@
 import Constant from "@/common/Constant";
 import { DownlinkType, UplinkType } from "@/common/types/satelliteSettingTypes";
-import TransceiverUtil from "@/common/util/TransceiverUtil";
 import ApiActiveSat from "@/renderer/api/ApiActiveSat";
 import ApiAppConfig from "@/renderer/api/ApiAppConfig";
 import ApiAppConfigSatellite from "@/renderer/api/ApiAppConfigSatellite";
@@ -220,39 +219,27 @@ export default class ActiveSatServiceHub {
     if (appConfigSatellite) {
       if (appConfigSatellite.autoModeUplinkFreq === 2) {
         // アップリンク設定が2の場合は、設定2を使用する
-        // TODO 変数名はHzだが実際はMhzなのでシステムがHzで扱うようになったらmhzToHzを削除する
         this.uplinkFreq = {
-          uplinkHz: appConfigSatellite.uplink2.uplinkHz
-            ? TransceiverUtil.mhzToHz(appConfigSatellite.uplink2.uplinkHz)
-            : null,
+          uplinkHz: appConfigSatellite.uplink2.uplinkHz ? appConfigSatellite.uplink2.uplinkHz : null,
           uplinkMode: appConfigSatellite.uplink2.uplinkMode,
         };
       } else {
         // それ以外は設定1を使用する
         this.uplinkFreq = {
-          // TODO 変数名はHzだが実際はMhzなのでシステムがHzで扱うようになったらmhzToHzを削除する
-          uplinkHz: appConfigSatellite.uplink1.uplinkHz
-            ? TransceiverUtil.mhzToHz(appConfigSatellite.uplink1.uplinkHz)
-            : null,
+          uplinkHz: appConfigSatellite.uplink1.uplinkHz ? appConfigSatellite.uplink1.uplinkHz : null,
           uplinkMode: appConfigSatellite.uplink1.uplinkMode,
         };
       }
       if (appConfigSatellite.autoModeDownlinkFreq === 2) {
         // ダウンリンク設定が2の場合は、設定2を使用する
         this.downlinkFreq = {
-          // TODO 変数名はHzだが実際はMhzなのでシステムがHzで扱うようになったらmhzToHzを削除する
-          downlinkHz: appConfigSatellite.downlink2.downlinkHz
-            ? TransceiverUtil.mhzToHz(appConfigSatellite.downlink2.downlinkHz)
-            : null,
+          downlinkHz: appConfigSatellite.downlink2.downlinkHz ? appConfigSatellite.downlink2.downlinkHz : null,
           downlinkMode: appConfigSatellite.downlink2.downlinkMode,
         };
       } else {
         // それ以外は設定1を使用する
         this.downlinkFreq = {
-          // TODO 変数名はHzだが実際はMhzなのでシステムがHzで扱うようになったらmhzToHzを削除する
-          downlinkHz: appConfigSatellite.downlink1.downlinkHz
-            ? TransceiverUtil.mhzToHz(appConfigSatellite.downlink1.downlinkHz)
-            : null,
+          downlinkHz: appConfigSatellite.downlink1.downlinkHz ? appConfigSatellite.downlink1.downlinkHz : null,
           downlinkMode: appConfigSatellite.downlink1.downlinkMode,
         };
       }
