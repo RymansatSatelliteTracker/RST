c8e3291802bd65163051ccfc54e2c12bffecd6a9
loikp4 <mineaki123@gmail.com>
2025-05-11 22:12:15 +0900

    zantei

--- src/__tests__/playwright/startup.test.ts
+++ src/__tests__/playwright/startup.test.ts
@@ -1,11 +1,20 @@
-// test/sample.test.ts
+import Constant from "@/common/Constant";
 import { _electron, ElectronApplication, expect, Page, test } from "@playwright/test";
+import Store from 'electron-store';
 
 let electronApp: ElectronApplication;
 let page: Page;
 
+const AppConfigStore = new Store({ name: Constant.Config.CONFIG_FILENAME });
+const WinodwStore = new Store({ name: Constant.Config.CONFIG_WINDOW });
+
 //electoronのアプリを開く
 test.beforeEach(async () => {
+  // electron-storeのデータをクリア
+  AppConfigStore.clear();
+  WinodwStore.clear();
+
+  // 起動
   electronApp = await _electron.launch({
     args: ["."],
   });
@@ -30,3 +39,26 @@ test("起動チェック", async () => {
   expect(height).toBeGreaterThan(0);
 
 });
+
+test("衛星のクリック", async () => {
+  // const satellite = await page.$('div.menu__item:has(span:has-text("衛星"))')
+  const satellite = await page.locator('div').filter({ hasText: /^衛星$/ }).getByRole('img').click();
+
+
+  /*const SatelliteSettingWindow = await satellite?.click();
+  expect(SatelliteSettingWindow).not.toBeNull();*/
+
+  // 表示されるまで待ち時間を設ける
+  await page.waitForTimeout(1000);
+  // 衛星の追加
+  const firstSatelliteData = await page.locator('.v-virtual-scroll').first().click();
+  console.log(firstSatelliteData)
+
+  // 表示されるまで待ち時間を設ける
+  await page.waitForTimeout(1000);
+
+  await page.locator('.v-row > div:nth-child(2)').first().click()
+  await page.waitForTimeout(1000);
+  await page.getByRole('button', { name: 'OK' }).click();;
+  await page.waitForTimeout(1000);
+  })
