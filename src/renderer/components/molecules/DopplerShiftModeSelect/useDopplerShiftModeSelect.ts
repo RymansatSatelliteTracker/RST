600850050863c466d0adc32316f061f9d3c061f5
freefloatingplanet <suenaganu@gmail.com>
2025-06-15 07:25:29 +0900

    定数修正の水平展開もれ

--- src/renderer/components/molecules/DopplerShiftModeSelect/useDopplerShiftModeSelect.ts
+++ src/renderer/components/molecules/DopplerShiftModeSelect/useDopplerShiftModeSelect.ts
@@ -4,8 +4,8 @@ import I18nUtil from "@/renderer/common/util/I18nUtil";
 
 export default function createDopplerShiftLabelMapping(): Record<string, string> {
   return {
-    [Constant.DopplerShiftMode.FIXED_SAT]: I18nUtil.getMsg(I18nMsgs.G2_FIXED_SAT),
-    [Constant.DopplerShiftMode.FIXED_RX]: I18nUtil.getMsg(I18nMsgs.G2_FIXED_RX),
-    [Constant.DopplerShiftMode.FIXED_TX]: I18nUtil.getMsg(I18nMsgs.G2_FIXED_TX),
+    [Constant.Transceiver.DopplerShiftMode.FIXED_SAT]: I18nUtil.getMsg(I18nMsgs.G2_FIXED_SAT),
+    [Constant.Transceiver.DopplerShiftMode.FIXED_RX]: I18nUtil.getMsg(I18nMsgs.G2_FIXED_RX),
+    [Constant.Transceiver.DopplerShiftMode.FIXED_TX]: I18nUtil.getMsg(I18nMsgs.G2_FIXED_TX),
   };
 }
