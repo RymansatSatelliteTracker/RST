de9e36f317b143c5dbfc3525d097a40e7b2e740f
freefloatingplanet <suenaganu@gmail.com>
2025-09-21 10:52:05 +0900

    テスト修正

--- src/__tests__/main/service/AppConfigSatelliteService.test.ts
+++ src/__tests__/main/service/AppConfigSatelliteService.test.ts
@@ -1,4 +1,5 @@
 import { AppConfigSatellite } from "@/common/model/AppConfigModel";
+import { DefaultSatelliteModel } from "@/common/model/DefaultSatelliteModel";
 import { createDefaultSatellite } from "@/common/util/DefaultSatelliteUtil";
 import AppConfigSatelliteService from "@/main/service/AppConfigSatelliteService";
 import DefaultSatelliteService from "@/main/service/DefaultSatelliteService";
@@ -9,13 +10,14 @@ const DEFAULT_NORAD_ID = "00000";
 const APPCONFIG_SATELLITE_ID = 1;
 const APPCONFIG_NORAD_ID1 = "12345";
 const APPCONFIG_NORAD_ID2 = "54321";
+const APPCONFIG_NORAD_ID3 = "99999";
 describe("AppConfigSatelliteService", () => {
   beforeAll(() => {
     jest.spyOn(DefaultSatelliteService.prototype, "init").mockImplementation(() => {
       return;
     });
     jest
-      .spyOn(DefaultSatelliteService.prototype, "getDefaultSatelliteBySatelliteIdSync")
+      .spyOn(DefaultSatelliteModel.prototype, "getDefaultSatelliteBySatelliteId")
       .mockImplementation((satelliteId: number) => {
         return createDefaultSatellite(satelliteId, "TEST_SAT", DEFAULT_NORAD_ID);
       });
@@ -28,7 +30,11 @@ describe("AppConfigSatelliteService", () => {
       sat2.satelliteId = APPCONFIG_SATELLITE_ID;
       sat2.noradId = APPCONFIG_NORAD_ID2;
       sat2.groupId = 2;
-      return { satellites: [sat1, sat2] } as any;
+      const sat3: AppConfigSatellite = new AppConfigSatellite();
+      sat3.satelliteId = APPCONFIG_SATELLITE_ID;
+      sat3.noradId = APPCONFIG_NORAD_ID3;
+      sat3.groupId = -1;
+      return { satellites: [sat1, sat2, sat3] } as any;
     });
   });
   it("デフォルト衛星を取得", () => {
@@ -49,13 +55,13 @@ describe("AppConfigSatelliteService", () => {
     // 検証
     expect(result?.noradId).toBe(APPCONFIG_NORAD_ID2);
   });
-  it("アプリケーション設定を取得(グループ不一致なので最初にヒットした設定)", () => {
+  it("アプリケーション設定を取得(グループ不一致なのでデフォルト衛星取得)", () => {
     // 準備
     const appConfigSatelliteService = new AppConfigSatelliteService();
     const groupId = 3;
     // 実行
     const result = appConfigSatelliteService.getUserRegisteredAppConfigSatellite(APPCONFIG_SATELLITE_ID, groupId);
     // 検証
-    expect(result?.noradId).toBe(APPCONFIG_NORAD_ID1);
+    expect(result?.noradId).toBe(APPCONFIG_NORAD_ID3);
   });
 });
