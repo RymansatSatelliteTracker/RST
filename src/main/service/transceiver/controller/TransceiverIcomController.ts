6c7eb5e9a441793d8d3131a4d27592d7f2ac455d
rsp-daigo <rsp.daigo@gmail.com>
2026-01-02 01:27:06 +0900

    AutoOn時にサテライトモードの要求が同時に２回発生する挙動の対処

--- src/main/service/transceiver/controller/TransceiverIcomController.ts
+++ src/main/service/transceiver/controller/TransceiverIcomController.ts
@@ -156,7 +156,7 @@ export default class TransceiverIcomController extends TransceiverSerialControll
     rxModeText: string,
     toneHz: number | null
   ): Promise<void> {
-    AppMainLogger.info(`無線機Auto On処理を開始します。`);
+    AppMainLogger.info(`無線機Auto On処理を開始します。 Rx：${rxModeText} Tx：${txModeText}`);
 
     // RST側の周波数を保存する（バンド入れ替え判定で必要）
     this.state.setReqRxFreqHz(rxFreqHz);
@@ -600,13 +600,17 @@ export default class TransceiverIcomController extends TransceiverSerialControll
       return;
     }
 
+    // 同じ値が既に設定されている場合はスキップ
+    if (this.state.isSatelliteMode === isSatelliteMode) {
+      return;
+    }
+    // サテライトモードの設定を保持する
+    this.state.isSatelliteMode = isSatelliteMode;
+
     // データ送信
     const cmdData = this.cmdMaker.makeSetSatelliteMode(isSatelliteMode);
     await this.sendAndWaitRecv(cmdData, "SET_MODE");
     AppMainLogger.debug(`サテライトモード（RST→無線機） ${isSatelliteMode}`);
-
-    // サテライトモードの設定を保持する
-    this.state.isSatelliteMode = isSatelliteMode;
 
     // サテライトモードの周波数、モードを取得する
     await this.getFreqFromIcom();
