d68de5dcd70203162c95cc49743fe0dcc666e293
freefloatingplanet <suenaganu@gmail.com>
2026-01-12 07:01:07 +0900

    トーン周波数をリストボックス化

---
+++ src/renderer/components/molecules/ToneFrequencySelect/ToneFrequencySelect.vue
@@ -0,0 +1,25 @@
+<!-- トーン周波数SelectBox -->
+<template>
+  <v-select
+    v-model="toneFrequency"
+    :items="ToneFrequencyRange"
+    item-title="title"
+    item-value="value"
+    no-data-text=""
+    density="compact"
+    variant="outlined"
+    hide-details
+    suffix="Hz"
+    class="selectbox"
+  ></v-select>
+</template>
+
+<script setup lang="ts">
+import Constant from "@/common/Constant";
+
+// 指定なしを選択可能にする
+const ToneFrequencyRange = [null, ...Constant.Transceiver.ToneFrequency];
+const toneFrequency = defineModel<number>("toneFrequency");
+</script>
+
+<style lang="scss" scoped></style>
