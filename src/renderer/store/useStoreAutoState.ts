f147c5331bddca884830064b4cd0f7058e69711c
rsp-daigo <rsp.daigo@gmail.com>
2026-02-09 01:12:45 +0900

    無線機のAutoOnの状態でもレーダー部のダブルクリックでローテータ移動を可能とする

--- src/renderer/store/useStoreAutoState.ts
+++ src/renderer/store/useStoreAutoState.ts
@@ -12,11 +12,21 @@ export const useStoreAutoState = defineStore(
     // ローテータのAuto状態
     const rotatorAuto = ref(false);
 
+    /**
+     * 無線機とローテータのいずれかがAutoOnか判定する
+     */
     function isAutoMode() {
       return rotatorAuto.value || tranceiverAuto.value;
     }
 
-    return { rotatorAuto, tranceiverAuto, isAutoMode };
+    /**
+     * ローテータがAutoOnか判定する
+     */
+    function isRotatorAutoMode() {
+      return rotatorAuto.value;
+    }
+
+    return { rotatorAuto, tranceiverAuto, isAutoMode, isRotatorAutoMode };
   },
   {
     // 再起動時の状態の保持は不要
