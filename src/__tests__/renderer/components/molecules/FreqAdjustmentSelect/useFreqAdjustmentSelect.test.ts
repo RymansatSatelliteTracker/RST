dd8029fc92b968e019d60ba45a3438ef1eca3b06
freefloatingplanet <suenaganu@gmail.com>
2026-03-09 21:49:43 +0900

    テストケース作成

--- src/__tests__/renderer/components/molecules/FreqAdjustmentSelect/useFreqAdjustmentSelect.test.ts
+++ src/__tests__/renderer/components/molecules/FreqAdjustmentSelect/useFreqAdjustmentSelect.test.ts
@@ -8,109 +8,111 @@ describe("useFrequencySelect", () => {
     return { frequencyRef, controller };
   }
 
-  it("周波数文字列をMHz/kHz/Hzの桁配列へ分割できる", () => {
+  it.each([
+    ["プラス", "+800.000", [8, 0, 0], [0, 0, 0]],
+    ["マイナス", "-700.000", [7, 0, 0], [0, 0, 0]],
+  ])("(%s)符号付き周波数文字列をkHz/Hzの桁配列へ分割できる", (name, frequency, expectedkHz, expectedHz) => {
     // Arrange
-    const { controller } = createSut("145.800.000");
+    const { controller } = createSut(frequency);
 
     // Act
 
     // Assert
-    expect(controller.mHzDigits.value).toEqual([1, 4, 5]);
-    expect(controller.kHzDigits.value).toEqual([8, 0, 0]);
-    expect(controller.hzDigits.value).toEqual([0, 0, 0]);
+    expect(controller.kHzDigits.value).toEqual(expectedkHz);
+    expect(controller.hzDigits.value).toEqual(expectedHz);
   });
 
-  it("onClickで指定桁を+1し、必要なら繰り上がる", () => {
+  it.each([
+    ["プラス", "+899.000", 2, "+900.000"],
+    ["マイナス", "-899.000", 2, "-900.000"],
+  ])("(%s)符号付き周波数文字列の指定桁を+1し、必要なら繰り上がる", (name, frequency, digit, expected) => {
     // Arrange
-    const { controller } = createSut("145.899.000");
+    const { controller } = createSut(frequency);
 
     // Act
-    const newFrequency = controller.onClick(5);
+    const newFrequency = controller.onClick(digit);
 
     // Assert
-    expect(newFrequency).toBe("145.900.000");
+    expect(newFrequency).toBe(expected);
   });
 
-  it("onWheelの上回転で指定桁を+1できる", () => {
+  it.each([
+    ["プラス", "+800.000", 3, "+800.100"],
+    ["マイナス", "-800.000", 3, "-799.900"],
+  ])("(%s)onWheelの上回転で指定桁を+1できる", (name, frequency, digit, expected) => {
     // Arrange
-    const { controller } = createSut("145.800.000");
+    const { controller } = createSut(frequency);
 
     // Act
-    const newFrequency = controller.onWheel({ deltaY: -1 } as WheelEvent, 5);
+    const newFrequency = controller.onWheel({ deltaY: -1 } as WheelEvent, digit);
 
     // Assert
-    expect(newFrequency).toBe("145.801.000");
+    expect(newFrequency).toBe(expected);
   });
 
-  it("onWheelの下回転で指定桁を-1し、必要なら繰り下がる", () => {
+  it.each([
+    ["プラス", "+800.000", 2, "+799.000"],
+    ["マイナス", "-800.000", 2, "-801.000"],
+  ])("(%s)onWheelの下回転で指定桁を-1し、必要なら繰り下がる", (name, frequency, digit, expected) => {
     // Arrange
-    const { controller } = createSut("145.800.000");
+    const { controller } = createSut(frequency);
 
     // Act
-    const newFrequency = controller.onWheel({ deltaY: 1 } as WheelEvent, 4);
+    const newFrequency = controller.onWheel({ deltaY: 1 } as WheelEvent, digit);
 
     // Assert
-    expect(newFrequency).toBe("145.790.000");
+    expect(newFrequency).toBe(expected);
   });
 
-  it("onWheelの下回転で先頭ゼロ領域を変更しようとするとnullを返す", () => {
+  it.each([
+    ["プラス", "+000.100", 2],
+    ["マイナス", "-000.100", 2],
+  ])("(%s)onWheelの下回転で先頭ゼロ領域を変更しようとするとnullを返す", (name, frequency, digit) => {
     // Arrange
-    const { controller } = createSut("000.100.000");
+    const { controller } = createSut(frequency);
 
     // Act
-    const newFrequency = controller.onWheel({ deltaY: 1 } as WheelEvent, 2);
+    const newFrequency = controller.onWheel({ deltaY: 1 } as WheelEvent, digit);
 
     // Assert
     expect(newFrequency).toBeNull();
   });
 
-  it("onWheelの下回転で全桁0になる場合はnullを返す", () => {
+  it.each([
+    ["プラス", "+000.001", 1, 8],
+    ["マイナス", "-000.001", -1, 8],
+  ])("(%s)onWheelで全桁0になる場合は0を返す", (name, frequency, wheelDirection, digit) => {
     // Arrange
-    const { controller } = createSut("000.000.001");
+    const { controller } = createSut(frequency);
 
     // Act
-    const newFrequency = controller.onWheel({ deltaY: 1 } as WheelEvent, 8);
+    const newFrequency = controller.onWheel({ deltaY: wheelDirection } as WheelEvent, digit);
 
     // Assert
-    expect(newFrequency).toBeNull();
+    expect(newFrequency).toBe("+000.000");
   });
 
-  it("onRightClickで指定桁以降を0にできる", () => {
+  it.each([
+    ["+876.543", 1, "+800.000"],
+    ["+876.543", 0, "+000.000"],
+    ["+006.543", 1, "+000.000"],
+    ["-876.543", 1, "-800.000"],
+    ["-876.543", 0, "+000.000"],
+    ["-006.543", 1, "+000.000"],
+  ])("onRightClickで指定桁以降を0にできる", (frequency, digit, expected) => {
     // Arrange
-    const { controller } = createSut("145.876.543");
+    const { controller } = createSut(frequency);
 
     // Act
-    const newFrequency = controller.onRightClick(4);
+    const newFrequency = controller.onRightClick(digit);
 
     // Assert
-    expect(newFrequency).toBe("145.800.000");
-  });
-
-  it("onRightClickで最大桁を指定した場合はnullを返す", () => {
-    // Arrange
-    const { controller } = createSut("145.876.543");
-
-    // Act
-    const newFrequency = controller.onRightClick(0);
-
-    // Assert
-    expect(newFrequency).toBeNull();
-  });
-
-  it("onRightClickで変更対象より上位桁が全て0の場合はnullを返す", () => {
-    // Arrange
-    const { controller } = createSut("000.120.340");
-
-    // Act
-    const newFrequency = controller.onRightClick(3);
-
-    // Assert
-    expect(newFrequency).toBeNull();
+    expect(newFrequency).toBe(expected);
   });
 
   it("isGrayedは先頭の非0桁より左側だけtrueを返す", () => {
     // Arrange
-    const { controller } = createSut("000.120.000");
+    const { controller } = createSut("000.120");
 
     // Act
     const isGrayedAt0 = controller.isGrayed(0);
