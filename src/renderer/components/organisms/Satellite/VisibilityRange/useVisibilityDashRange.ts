a4fc2e52f641f48639a2d761b7c5952da2a152f2
stargazer1846923 <kondorymansat@gmail.com>
2026-05-18 23:17:40 +0900

    fix: 地図をはみ出して可視範囲が表示される不具合を修正

--- src/renderer/components/organisms/Satellite/VisibilityRange/useVisibilityDashRange.ts
+++ src/renderer/components/organisms/Satellite/VisibilityRange/useVisibilityDashRange.ts
@@ -46,7 +46,17 @@ export default function useVisibilityDashRange(currentDate: Ref<Date>, offsetLon
             location.longitude +
             (visibleAngleRad / (lengthOfOneDegree * Math.cos(CoordinateCalcUtil.degreeToRadian(latitude)))) *
               Math.sin(radian);
-          resolve([latitude, longitude]);
+
+          if (latitude > 90) {
+            // 緯度が90度を超える場合は90度に制限する
+            resolve([90, longitude]);
+          } else if (latitude < -90) {
+            // 緯度が-90度を下回る場合は-90度に制限する
+            resolve([-90, longitude]);
+          } else {
+            // 緯度が-90度から90度の範囲内の場合はそのまま使用する
+            resolve([latitude, longitude]);
+          }
         });
       });
 
